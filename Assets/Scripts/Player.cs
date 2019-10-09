6617ecbe8c909b3cfe4a75ff2cc1322e8bac81e3
quangChe <quang.n.che@gmail.com>
2019-10-09 12:33:38 -0700

    Added prefabs and configuration parameters that set padding on the viewport

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,11 +4,31 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    // Configuration parameters
     [SerializeField] float MoveSpeed = 10f;
+    [SerializeField] float xPadding = 0.6f;
+    [SerializeField] float yPaddingTopPercentage = 0.3f;
+    [SerializeField] float yPaddingBottom = 0.5f;
+
+    float xMin;
+    float xMax;
+    float yMin;
+    float yMax;
 
     // Start is called before the first frame update
     void Start()
     {
+        SetMoveBoundaries();
+    }
+
+    private void SetMoveBoundaries()
+    {
+        Debug.Log(xPadding);
+        Camera gameCamera = Camera.main;
+        xMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x + xPadding;
+        xMax = gameCamera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x - xPadding;
+        yMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y + yPaddingBottom;
+        yMax = gameCamera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y * yPaddingTopPercentage;
     }
 
     // Update is called once per frame
@@ -21,9 +41,10 @@ public class Player : MonoBehaviour
     {
         var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * MoveSpeed;
         var deltaY = Input.GetAxis("Vertical") * Time.deltaTime * MoveSpeed;
-        var newXPos = transform.position.x + deltaX;
-        var newYPos = transform.position.y + deltaY;
-        transform.position = new Vector2(newXPos, newYPos);
 
+        var newXPos = Mathf.Clamp(transform.position.x + deltaX, xMin, xMax);
+        var newYPos = Mathf.Clamp(transform.position.y + deltaY, yMin, yMax);
+
+        transform.position = new Vector2(newXPos, newYPos);
     }
 }
