a486d31d163f83c2f787dfd98b1916de71f120d4
quangChe <quang.n.che@gmail.com>
2019-10-09 15:25:13 -0700

    Combine WaveConfig with new path prefabs to customize paths for enemy prefabs.

--- Assets/Scripts/EnemyPath.cs
+++ Assets/Scripts/EnemyPath.cs
@@ -4,14 +4,17 @@ using UnityEngine;
 
 public class EnemyPath : MonoBehaviour
 {
-    [SerializeField] List<Transform> waypoints;
+    [SerializeField] WaveConfig waveConfig;
     [SerializeField] float moveSpeed = 5f;
+
+    List<Transform> waypoints;
 
     int waypointIndex = 0;
 
     // Start is called before the first frame update
     void Start()
     {
+        waypoints = waveConfig.GetWaypoints();
         transform.position = waypoints[waypointIndex].transform.position;
     }
 
