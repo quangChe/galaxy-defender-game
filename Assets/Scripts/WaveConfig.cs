a12aa00162c61ed108d3bdbc881fb201cd087321
quangChe <quang.n.che@gmail.com>
2019-10-09 15:07:11 -0700

    Added a WaveConfig script to store configurations on a wave of enemies (movement path, sprite, spawn times, quantity, etc.)

--- Assets/Scripts/WaveConfig.cs
+++ Assets/Scripts/WaveConfig.cs
@@ -5,15 +5,23 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Enemy Wave Config")]
 public class WaveConfig : ScriptableObject
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] GameObject enemyPrefab;
+    [SerializeField] GameObject pathPrefab;
+    [SerializeField] float timeBetweenSpawns = 0.5f;
+    [SerializeField] float spawnRandomFactor = 0.3f;
+    [SerializeField] int numberOfEnemies = 5;
+    [SerializeField] float moveSpeed = 2f;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public GameObject GetEnemyPrefab() { return enemyPrefab; }
+
+    public GameObject GetPathPrefab() { return pathPrefab; }
+
+    public float TimeBetweenSpawns() { return timeBetweenSpawns; }
+
+    public float SpawnRandomFactor() { return spawnRandomFactor; }
+
+    public int NumberOfEnemies() { return numberOfEnemies; }
+
+    public float MoveSpeed() { return moveSpeed; }
+
 }
