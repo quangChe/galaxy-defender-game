3ad999c960bd46d25c32db492130d2133eff9474
quangChe <quang.n.che@gmail.com>
2019-10-09 11:18:03 -0700

    Added Script for player and a .gitignore file. Also added functionality to move the player ship using up, down, right, left buttons.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] float MoveSpeed = 10f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Move();
+    }
+
+    private void Move()
+    {
+        var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * MoveSpeed;
+        var deltaY = Input.GetAxis("Vertical") * Time.deltaTime * MoveSpeed;
+        var newXPos = transform.position.x + deltaX;
+        var newYPos = transform.position.y + deltaY;
+        transform.position = new Vector2(newXPos, newYPos);
+
     }
 }
